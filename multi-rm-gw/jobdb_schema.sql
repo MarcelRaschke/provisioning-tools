facbda4a47bde2272aad6744e52cd744ca5b38f2
Radu Popescu <mail@radupopescu.net>
2019-01-22 18:00:00 +0100

    Adding config file templates for Conveyor

---
+++ multi-rm-gw/jobdb_schema.sql
@@ -0,0 +1,18 @@
+USE {{ db_name }};
+
+CREATE TABLE IF NOT EXISTS Jobs (
+    ID char(36) NOT NULL UNIQUE PRIMARY KEY,
+    JobName varchar(65535) NOT NULL,
+    Repository varchar(65535) NOT NULL,
+    Payload varchar(65535) NOT NULL,
+    RepositoryPath varchar(65535) NOT NULL,
+    Script varchar(65535) NOT NULL,
+    ScriptArgs varchar(65535) NOT NULL,
+    TransferScript boolean NOT NULL,
+    Dependencies varchar(65535) NOT NULL,
+    WorkerName varchar(65535) NOT NULL,
+    StartTime timestamp NOT NULL,
+    FinishTime timestamp NOT NULL,
+    Successful boolean NOT NULL,
+    ErrorMessage varchar(65535) NOT NULL
+);
